fa1a40e3f0b0b8e3acee437f2b073ac3cec95ebe
jashanwarraich <jsingh3_be19@thapar.edu>
2022-06-19 18:36:31 +0530

    q11

--- RepeatAndMissingNumber.cpp
+++ RepeatAndMissingNumber.cpp
@@ -1,5 +1,96 @@
 //https://www.codingninjas.com/codestudio/problems/873366?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
 
+
+
+// https://www.geeksforgeeks.org/find-a-repeating-and-a-missing-number/
+// C++ program to Find the repeating
+// and missing elements
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* The output of this function is stored at
+*x and *y */
+void getTwoElements(int arr[], int n,
+                    int* x, int* y)
+{
+    /* Will hold xor of all elements
+    and numbers from 1 to n */
+    int xor1;
+ 
+    /* Will have only single set bit of xor1 */
+    int set_bit_no;
+ 
+    int i;
+    *x = 0;
+    *y = 0;
+ 
+    xor1 = arr[0];
+ 
+    /* Get the xor of all array elements */
+    for (i = 1; i < n; i++)
+        xor1 = xor1 ^ arr[i];
+ 
+    /* XOR the previous result with numbers
+    from 1 to n*/
+    for (i = 1; i <= n; i++)
+        xor1 = xor1 ^ i;
+ 
+    /* Get the rightmost set bit in set_bit_no */
+    set_bit_no = xor1 & ~(xor1 - 1);
+ 
+    /* Now divide elements into two
+    sets by comparing a rightmost set
+    bit of xor1 with the bit at the same
+    position in each element. Also,
+    get XORs of two sets. The two
+    XORs are the output elements.
+    The following two for loops
+    serve the purpose */
+    for (i = 0; i < n; i++) {
+        if (arr[i] & set_bit_no)
+            /* arr[i] belongs to first set */
+            *x = *x ^ arr[i];
+ 
+        else
+            /* arr[i] belongs to second set*/
+            *y = *y ^ arr[i];
+    }
+    for (i = 1; i <= n; i++) {
+        if (i & set_bit_no)
+            /* i belongs to first set */
+            *x = *x ^ i;
+ 
+        else
+            /* i belongs to second set*/
+            *y = *y ^ i;
+    }
+ 
+    /* *x and *y hold the desired
+        output elements */
+}
+ 
+/* Driver code */
+int main()
+{
+    int arr[] = { 1, 3, 4, 5, 5, 6, 2 };
+    int* x = (int*)malloc(sizeof(int));
+    int* y = (int*)malloc(sizeof(int));
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    getTwoElements(arr, n, x, y);
+    cout << " The missing element is " << *x << " and the repeating"
+         << " number is " << *y;
+    getchar();
+}
+ 
+// This code is contributed by Code_Mech
+
+
+
+
+//space: O(n)
+// Time O(n)
 #include <bits/stdc++.h> 
 
 pair<int,int> missingAndRepeating(vector<int> &a, int n)
@@ -27,3 +118,30 @@ pair<int,int> missingAndRepeating(vector<int> &a, int n)
     
 }
 
+
+// Space: O(1)
+// Time: O(nlog(n))
+// No extra space used but changed the original array
+
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &a, int n)
+{
+    pair<int,int> p;
+
+    sort(a.begin(),a.end());
+    int rs=(n*(n+1))/2;
+    int s=0;
+    int i;
+    for(i=0;i<n-1;i++){
+        s+=a[i];
+        if(a[i]==a[i+1]){
+            p.second=a[i];
+        }       
+    }
+    s+=a[i];
+//     cout<<s<<" ";
+//     cout<<rs<<" ";
+    p.first=rs-(s-p.second);
+    return p;
+}
