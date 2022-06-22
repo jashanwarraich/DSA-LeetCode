7680c34ab95ceb92cfdf1572b5cbcc343f687ba6
jashanwarraich <jsingh3_be19@thapar.edu>
2022-06-22 23:51:28 +0530

    q33

---
+++ ReverseALinkedListInGroupsOfSizeK.cpp
@@ -0,0 +1,32 @@
+// Q33 https://leetcode.com/problems/reverse-nodes-in-k-group/
+// O((N/k)*k)
+
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* h, int k) {
+        if(!h||k==1) return h;
+        ListNode* dummy=new ListNode;
+        dummy->next=h;
+        ListNode *c=dummy, *p=dummy, *n=dummy;
+        int cnt=0;
+        while(c->next){
+            cnt++;
+            c=c->next;
+        }
+        
+        while(cnt>=k){
+            c=p->next;
+            n=c->next;
+            for(int i=1;i<k;i++){
+                c->next=n->next;
+                n->next=p->next;
+                p->next=n;
+                n=c->next;
+            }
+            p=c;
+            cnt-=k;
+        }
+        return dummy->next;
+    }
+};
