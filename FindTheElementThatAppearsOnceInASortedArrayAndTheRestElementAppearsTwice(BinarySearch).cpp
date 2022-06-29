741980dea95975d2fd98f9e1b7614d96afc65ab6
jashanwarraich <jsingh3_be19@thapar.edu>
2022-06-29 19:12:55 +0530

    q63

--- FindTheElementThatAppearsOnceInASortedArrayAndTheRestElementAppearsTwice(BinarySearch).cpp
+++ FindTheElementThatAppearsOnceInASortedArrayAndTheRestElementAppearsTwice(BinarySearch).cpp
@@ -1,8 +1,15 @@
 // Q63 https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/
 
+// Approach 1
 // Time: O(n)
 // Space: O(1)
+// OR every element with the previous element, return it if its not 0  
 
+
+
+// Approach 2: Binary Search
+// Time: O(logn)
+// Space: O(1)
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -10,7 +17,7 @@ public:
         int h=nums.size()-2;
         
         while(h>=l){
-            int mid=(l+h)/2;
+            int mid=(l+h)/2;     //int mid=(l+h)>>1; 
             if(nums[mid]==nums[mid^1])
                 l=mid+1;
             else
