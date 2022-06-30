547faf6c42de9e3d47e096fe9822561bc3562adf
jashanwarraich <jsingh3_be19@thapar.edu>
2022-06-30 23:03:39 +0530

    q67

--- AllocateMinimumNumberOfPages.cpp
+++ AllocateMinimumNumberOfPages.cpp
@@ -3,11 +3,9 @@
 // Time: n*logn
 // Space: O(1)
 
-
-
-bool ispossible(int n, int m, vector <int> a, int mid){
+bool ispossible(int n, int m, vector <int> a, long long int mid){
     int cnt=0;
-    int sumal=0;
+    long long int sumal=0;
     for(int i=0;i<m;i++){
         if(sumal+a[i]>mid){
             cnt++;
@@ -19,39 +17,31 @@ bool ispossible(int n, int m, vector <int> a, int mid){
         }
     }
     if(cnt<n) return true;
-    else return false;
+    return false;
 }
 
-long long ayushGivesNinjatest(int n, int m, vector<int> a) {	
+
+long long ayushGivesNinjatest(int n, int m, vector<int> a) {
 	if(n>m) return -1;
-    int l=a[0];
-    int h=0;
+    long long l=a[0];
+    long long h=0;
     for(int i=0;i<m;i++){
-        l=min(l,a[i]);
+//         l=min(l,a[i]);
+        if(a[i]<l) l=a[i];
         h+=a[i];
     }
+    long long ans=-1;
     while(l<=h){
-        int mid=(l+h)>>1;
+        long long mid=(l+h)>>1;
         if(ispossible(n,m,a,mid)){
-            hhttps://www.codingninjas.com/codestudio/library=mid-1;
+            h=mid-1;
+            ans=mid;
         }
         else
             l=mid+1;
     }
-    return l;
+    return ans;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
