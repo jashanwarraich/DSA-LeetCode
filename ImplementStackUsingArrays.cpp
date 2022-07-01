e7305d247d04fc3482867562a02d5e01fbd69165
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-01 16:54:46 +0530

    extra

--- ImplementStackUsingArrays.cpp
+++ ImplementStackUsingArrays.cpp
@@ -1,23 +1,4 @@
-// Q75 https://www.codingninjas.com/codestudio/problems/max-xor-queries_1382020?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
 
-// Brute force 
-vector<int> maxXorQueries(vector<int> &a, vector<vector<int>> &q){
-    vector<int> ans;
-    int n=a.size();
-    for(int i=0;i<q.size();i++){
-//         int r=-1;
-        int ma=-1;
-        for(int j=0;j<n;j++){
-           if(a[j]<=q[i][1]){
-//                int val=a[j]^q[i][0];
-               ma=max(ma,a[j]^q[i][0]);
-//                cout<<val<<endl;
-           }
-        }
-        ans.push_back(ma);
-    }
-    return ans;
-}
 
 //Stack implementation
 
