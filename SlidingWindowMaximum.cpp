ab79943e7482266d2976622069f52b7b06779407
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-03 23:41:17 +0530

    q86

--- SlidingWindowMaximum.cpp
+++ SlidingWindowMaximum.cpp
@@ -1,5 +1,21 @@
 // Q86 https://leetcode.com/problems/sliding-window-maximum/
 
-Time: 
-Space:
-
+// Time: O(n)+O(n) amortised
+// Space: O(n)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& a, int k) {
+        int n=a.size();
+        deque<int> q;
+         vector<int> v;
+        for(int i=0;i<n;i++){
+            while(!q.empty()&&q.front()==i-k)
+                q.pop_front();
+            while(!q.empty()&&a[i]>=a[q.back()])
+                q.pop_back();
+            q.push_back(i);
+            if(i>=k-1) v.push_back(a[q.front()]);
+        }
+        return v;
+    }
+};
