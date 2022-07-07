ded36c947c7e7f7ece5969cf7dc9c27d41ced9f2
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-07 22:14:29 +0530

    q121

--- CheckIfTwoTreesAreIdenticalOrNot.cpp
+++ CheckIfTwoTreesAreIdenticalOrNot.cpp
@@ -6,7 +6,17 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL&&q==NULL) return true;
-        else if(q==NULL||p==NULL) return false;
+        else if(q==NULL||p==NULL) return false; // above and this line can be replaced by- if(p==NULL||q==NULL) return p==q;
+        return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+    }
+};
+
+
+//Same as above
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL||q==NULL) return p==q; //I any one is NULL, other should also be NULL, else return false
         return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
