3151a876c32457c17e3b37a769e145822432c0e3
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-07 21:03:52 +0530

    q118

--- DiameterOfBinaryTree.cpp
+++ DiameterOfBinaryTree.cpp
@@ -1,5 +1,25 @@
 // Q118 https://leetcode.com/problems/diameter-of-binary-tree/
 
+// Time: O(n)
+class Solution {
+public:
+    int h(TreeNode* root,int &ans) {    //height function modified to manage diameter and height in the same
+        if(!root) return 0;
+        int lh=h(root->left, ans);
+        int rh=h(root->right, ans);
+        ans=max(ans,lh+rh);
+        return 1+max(lh,rh);
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=0;
+        h(root,ans);
+        return ans;
+    }
+};
+
+
+// Time: O(n*n)
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
