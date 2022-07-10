ca53553ff05070eeff6cad6fec60462126928462
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-10 22:17:44 +0530

    q143

--- FindAPairWithAGivenSumInBST.cpp
+++ FindAPairWithAGivenSumInBST.cpp
@@ -1,5 +1,61 @@
 // Q142 https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
 
+Time: O(n)
+Space: O(h)+O(h)  to store i and j stack
+
+
+class BSTIterator{
+private:
+    stack<TreeNode*> s;
+    bool rev=true;
+    void pushAll(TreeNode* root){
+        while(root){
+            s.push(root);
+            if(!rev) root=root->left;
+            else root=root->right;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root, bool isReverse){
+        rev=isReverse;
+        pushAll(root);
+    }
+    
+    bool hasNext(){
+            return !s.empty();
+        }
+    
+    int next(){
+        TreeNode* t=s.top();
+        s.pop();
+        if(!rev) pushAll(t->right);
+        else pushAll(t->left);
+        return t->val;
+    }
+    
+};
+class Solution {
+    
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+        int i=l.next();
+        int j=r.next();
+        while(i<j){
+            if(k==i+j) return true;
+            if(k>i+j) i=l.next();
+            else j=r.next();
+        }
+        return false;
+    }
+};
+
+
+
+
+
 // Time: O(n)
 // Space: O(n) 
 
