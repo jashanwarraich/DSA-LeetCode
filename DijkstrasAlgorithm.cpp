04cf08d43266b5961ac3544f96a90ae60fe0d315
jashanwarraich <jsingh3_be19@thapar.edu>
2022-07-13 23:46:01 +0530

    q165

--- DijkstrasAlgorithm.cpp
+++ DijkstrasAlgorithm.cpp
@@ -20,49 +20,15 @@ vector<int> dijkstra(vector<vector<int>> &edge, int v, int e, int src) {
         q.pop();
 //         vector<pair<int,int>> :: iterator it;
         for(auto it:adj[prev]){
-            int next=q.top().first;
-            int nextdist=q.top().second;
+            int next=it.first;
+            int nextdist=it.second;
             if(dist[next]>dis+nextdist){
                 dist[next]=dis+nextdist;
                 q.push(make_pair(dist[next],next));
             }
         }
+        
+
     }
     return dist;
-}
-
-
-
-
-
-vector<int> dijkstra(vector<vector<int>> &vec, int v, int edges, int source) {
-    
-    vector<pair<int,int> > adj[v];
-    
-    for(int i=0;i<edges;i++)
-    {
-        adj[vec[i][0]].push_back({vec[i][1],vec[i][2]});
-        adj[vec[i][1]].push_back({vec[i][0],vec[i][2]});
-    }
-    
-    vector<int> arr(v,INT_MAX);
-    priority_queue< pair<int,int> , vector<pair<int,int> >, greater<pair<int,int> > > pq;
-    arr[source] = 0;
-    pq.push({0,source});
-    
-    while(pq.empty()==false)
-    {
-       int topnode = pq.top().second;
-        int dist = pq.top().first;
-        pq.pop();
-        for(auto neigh : adj[topnode])
-        {
-            if(dist + neigh.second < arr[neigh.first])
-            {
-                arr[neigh.first] = dist + neigh.second;
-                pq.push({arr[neigh.first],neigh.first});
-            }
-        }
-    }
-    return arr;
 }
